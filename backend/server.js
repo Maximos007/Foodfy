34978fdf3d21394980da925d1575e63594031969
Maximos007 <waslleymax@gmail.com>
2021-03-24 18:00:25 -0300

    v1.1.0

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname,'..', 'frontend', 'public', 'css')));
+app.use(express.static(path.join(__dirname,'..', 'frontend', 'public', 'assets')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'frontend', 'index.html'));
+});
+app.get('/sobre', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..', 'frontend', 'sobre.html'));
+});
+app.get('/receitas', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '..','frontend', 'receitas.html'));
+});
+
+app.listen(3333);
