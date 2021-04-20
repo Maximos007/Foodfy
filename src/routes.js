2c87e3c4c3168494037cbc78b1e6aa042361ccb3
Maximos007 <waslleymax@gmail.com>
2021-04-20 15:44:11 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const recipes = require('./data');
+
 const routes = express.Router();
 
 const views = __dirname + '/views/';
 
-routes.get('/', (req, res) => res.render(views + 'index'));
+routes.get('/', (req, res) => res.render(views + 'index', {recipes}));
 routes.get('/sobre', (req, res) => res.render(views + 'sobre'));
-routes.get('/receitas', (req, res) => res.render(views + 'receitas'));
+routes.get('/receitas', (req, res) => res.render(views + 'receitas', {recipes}));
+routes.get('/not-found', (req, res) => res.render(views + 'not-found'));
 
 module.exports = routes;
 
