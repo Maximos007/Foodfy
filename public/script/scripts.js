35c5337995d3967a2cc997ec813d12a31d7d29a7
Maximos007 <waslleymax@gmail.com>
2021-04-20 15:44:22 -0300

    Update scripts.js

--- public/script/scripts.js
+++ public/script/scripts.js
@@ -4,19 +4,21 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards){
 	card.addEventListener ('click' , function (){
 		const imageId = card.getAttribute('id');
-		const title = card.querySelector('.title_recipe').innerText; // atribui o texto que contém 
-		const author = card.querySelector('.card_chef').innerText;
+		window.location.href = '/';
 
-		modalOverlay.classList.add('active');
-		modalOverlay.querySelector('img').src = `/assets/${imageId}`;
-		modalOverlay.querySelector('.title-modal').innerHTML = `${title}`;
-		modalOverlay.querySelector('.chef-modal').innerHTML = `${author}`;
+		// const title = card.querySelector('.title_recipe').innerText; // atribui o texto que contém 
+		// const author = card.querySelector('.card_chef').innerText;
+
+		// modalOverlay.classList.add('active');
+		// modalOverlay.querySelector('img').src = `/assets/${imageId}`;
+		// modalOverlay.querySelector('.title-modal').innerHTML = `${title}`;
+		// modalOverlay.querySelector('.chef-modal').innerHTML = `${author}`;
 	});
 }
 
-const closeModal = document.querySelector('.close-modal');
+// const closeModal = document.querySelector('.close-modal');
 
-closeModal.addEventListener('click', function (){
-	modalOverlay.classList.remove('active'); // remove classe active p/ fechar modal
-	modalOverlay.querySelector('img').src = '';
-});
+// closeModal.addEventListener('click', function (){
+// 	modalOverlay.classList.remove('active'); // remove classe active p/ fechar modal
+// 	modalOverlay.querySelector('img').src = '';
+// });
