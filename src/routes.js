b943a30aa66e8468ca35111cb1b9ffb3344322ec
Maximos007 <waslleymax@gmail.com>
2021-04-30 16:49:33 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,17 @@ const routes = express.Router();
 const views = __dirname + '/views/';
 
 routes.get('/', (req, res) => res.render(views + 'index', {recipes}));
-routes.get('/sobre', (req, res) => res.render(views + 'sobre'));
-routes.get('/receitas', (req, res) => res.render(views + 'receitas', {recipes}));
-routes.get('/not-found', (req, res) => res.render(views + 'not-found'));
+routes.get('/about', (req, res) => res.render(views + 'about'));
+routes.get('/recipes', (req, res) => res.render(views + 'recipes', {recipes}));
+routes.get('/recipes/:index', (req, res) => {
+	const recipesIndex = req.params.index;
+	return res.render(views + 'recipe', {recipe: recipes[recipesIndex]});
+
+	// const recipeIndex = req.params.index;
+
+	// console.log(recipes[recipeIndex]);
+
+});
 
 module.exports = routes;
 
