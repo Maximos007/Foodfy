007af3b3b2b4e10529cb5df70f58324b79d1a4c5
turistakiller <78664165+turistakiller@users.noreply.github.com>
2024-03-04 15:07:55 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,9 @@ routes.get('/about', (req, res) => res.render(views + 'about'));
 routes.get('/recipes', (req, res) => res.render(views + 'recipes', {recipes}));
 routes.get('/recipes/:index', (req, res) => {
 	const recipesIndex = req.params.index;
-	return res.render(views + 'recipe', {recipe: recipes[recipesIndex]});
-
-	// const recipeIndex = req.params.index;
-
-	// console.log(recipes[recipeIndex]);
-
+	const recipe = recipes[recipesIndex];
+	if (!recipe) return res.status(404).render(`${views}not-found`);
+	return res.render(`${views}recipe`, {recipe});
 });
 
 module.exports = routes;
